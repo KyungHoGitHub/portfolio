1ef229325559512eabc05e3249a883bd25d504e1
KyungHoGitHub <>
2024-10-29 22:27:42 +0900

    :lipstick: 페이지 컴포넌트 레이아웃 수정

--- src/component/DashBoard.jsx
+++ src/component/DashBoard.jsx
@@ -1,4 +1,6 @@
-import {Card, Image, List, Typography} from "antd";
+import { Layout, Card, Image, List, Table, Typography, Row, Col } from "antd";
+
+const { Header, Content } = Layout;
 
 const data = [
     '유경호',
@@ -7,44 +9,64 @@ const data = [
     'Man charged over missing wedding girl.',
     'Los Angeles battles huge wildfires.',
 ];
-const DashBoard = () => {
-    return(
-        <>
-            <Card>
-            <Image
-                style={
-                {
-                    position: "relative", // 위치 조정 옵션
-                    left: "10px",
-                }
-                }
-                width={200}
-                preview={{
-                    destroyOnClose: true,
-                    imageRender: () => (
-                        <video
-                            muted
-                            width="100%"
-                            controls
-                            src="https://mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*uYT7SZwhJnUAAAAAAAAAAAAADgCCAQ"
-                        />
-                    ),
-                    toolbarRender: () => null,
-                }}
-                src="https://www.meconomynews.com/news/photo/202403/90179_110062_4024.jpg"
-            />
-            </Card>
 
-            <List
-                bordered
-                dataSource={data}
-                renderItem={(item) => (
-                    <List.Item>
-                        <Typography.Text mark>[ITEM]</Typography.Text> {item}
-                    </List.Item>
-                )}
-            />
-        </>
-    )
-}
+const DashBoard = () => {
+    return (
+        <Layout>
+            <Header style={{ backgroundColor: '#fff', padding: '0 20px', position: 'fixed', top: 0, width: '100%', zIndex: 1 }}>
+                <Row>
+                    <Col span={4}>
+                        <h1 style={{ left:0 }}>Make your portfolio</h1>
+                    </Col>
+                </Row>
+            </Header>
+
+            <Content style={{padding: '80px 20px 20px 20px', marginTop: '0px', backgroundColor: '#fff'}}>
+                <Row gutter={16}>
+                    {/* 이미지 섹션 */}
+                    <Col span={6}>
+                        <Image
+                            width="100%"
+                            preview={{
+                                destroyOnClose: true,
+                                imageRender: () => (
+                                    <video
+                                        muted
+                                        width="100%"
+                                        controls
+                                        src="https://mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*uYT7SZwhJnUAAAAAAAAAAAAADgCCAQ"
+                                    />
+                                ),
+                                toolbarRender: () => null,
+                            }}
+                            src="https://www.meconomynews.com/news/photo/202403/90179_110062_4024.jpg"
+                        />
+                    </Col>
+
+
+                    {/* 테이블 섹션 */}
+                    <Col span={12}>
+                        <Table style={{width: '100%'}}/>
+                    </Col>
+                </Row>
+                <div style={{height: '20px'}}/>
+                {/* 카드 섹션 */}
+                <Col span={6}>
+                    <Card style={{height: 450}}>
+                        <List
+                            bordered
+                            dataSource={data}
+                            renderItem={(item) => (
+                                <List.Item>
+                                    <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                                </List.Item>
+                            )}
+                        />
+                    </Card>
+                </Col>
+            </Content>
+        </Layout>
+    );
+};
+
 export default DashBoard;
--- src/component/SignUpPage.jsx
+++ src/component/SignUpPage.jsx
@@ -11,33 +11,36 @@ const SignUpPage = () => {
 
     return (
         <>
-            <div style={{top: 0, position: 'fixed', alignItems: 'center', margin: 0}}>
+            <div style={{top: 0, position: 'fixed', left: 0, alignItems: 'center', margin: 0}}>
                 <h1>
                     Make your portfolio
                 </h1>
             </div>
-            <Form
-                name="basic"
-                labelCol={{
-                    span: 8,
-                }}
-                wrapperCol={{
-                    span: 16,
-                }}
-                style={{
-                    maxWidth: 1600,
-                }}
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <Row>
+            <Row>
+                <Form
+                    name="basic"
+                    labelCol={{
+                        span: 8,
+                    }}
+                    wrapperCol={{
+                        span: 16,
+                    }}
+                    style={{
+                        maxWidth: 1600,
+                    }}
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+
                     <Form.Item
                         label="아이디"
                         name="id"
+
+
                         rules={[
                             {
                                 required: true,
@@ -47,62 +50,67 @@ const SignUpPage = () => {
                     >
                         <Input/>
                     </Form.Item>
-                    <Button>
-                        중복확인
-                    </Button>
-                </Row>
-                <Form.Item
-                    label="패스워드"
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your password!',
-                        },
-                    ]}
-                >
-                    <Input.Password/>
-                </Form.Item>
-                <Form.Item
-                    label="이름"
-                    name="name"
-                >
-                    <Input/>
-                </Form.Item>
-                <Form.Item
-                    label="이메일"
-                    name="email"
-                >
-                    <Input/>
-                </Form.Item>
-                <Form.Item
-                    label="휴대전화"
-                    name="phoneNumber"
-                >
-                    <Input/>
-                </Form.Item>
-                <Form.Item
-                    name="remember"
-                    valuePropName="checked"
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                    }}
-                >
 
-                </Form.Item>
+                    <Form.Item
+                        label="패스워드"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}
+                    >
+                        <Input.Password/>
+                    </Form.Item>
+                    <Form.Item
+                        label="이름"
+                        name="name"
+                    >
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item
+                        label="이메일"
+                        name="email"
+                    >
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item
+                        label="휴대전화"
+                        name="phoneNumber"
+                    >
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item
+                        name="remember"
+                        valuePropName="checked"
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
 
-                <Form.Item
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                    }}
+                    </Form.Item>
+
+                    <Form.Item
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            회원가입
+                        </Button>
+                    </Form.Item>
+                </Form>
+                <Button
+                style={{
+                    marginLeft: 10,
+                }}
                 >
-                    <Button type="primary" htmlType="submit">
-                        회원가입
-                    </Button>
-                </Form.Item>
-            </Form>
+                    중복확인
+                </Button>
+            </Row>
         </>
     )
 }
